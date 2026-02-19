a4537c1f11feb698a6a7559054804c929dce5661
Sir Waithaka <kennwaithaka@gmail.com>
2026-02-19 19:08:10 +0300

    Update stk query endpoint to v2 (#2)

--- daraja/constants.go
+++ daraja/constants.go
@@ -8,7 +8,7 @@ const (
 	EndpointC2bExpress        = "/mpesa/stkpush/v1/processrequest"
 	EndpointAccountBalance    = "/mpesa/accountbalance/v1/query"
 	EndpointReversal          = "/mpesa/reversal/v1/request"
-	EndpointC2bExpressQuery   = "/mpesa/stkpushquery/v1/query"
+	EndpointC2bExpressQuery   = "/mpesa/stkpushquery/v2/query"
 	EndpointTransactionStatus = "/mpesa/transactionstatus/v1/query"
 	EndpointB2cPayment        = "/mpesa/b2c/v3/paymentrequest"
 	EndpointB2bPayment        = "/mpesa/b2b/v1/paymentrequest"
